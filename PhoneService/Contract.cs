a936c7507b565aedefaf85d889d5d9fee4e1712c
Dima <DimaBrisset@gmail.com>
2022-07-16 14:47:38 +0300

    Добавьте файлы проекта.

---
+++ PhoneService/Contract.cs
@@ -0,0 +1,22 @@
+﻿namespace PhoneService
+{
+    internal class Contract
+    {
+        public Guid Id { get; set; }
+        public int PhoneNumber { get; set; }
+        public int TargetPhoneNimber { get; set; }
+        public DateTime StartCall { get; set; }
+        public DateTime EndCall { get; set; }
+        public int Amount { get; set; }
+
+        public Contract(int phoneNumber, int targetPhoneNumber, DateTime startCall)
+        {
+            Id = Guid.NewGuid();
+            PhoneNumber = phoneNumber;
+            TargetPhoneNimber = targetPhoneNumber;
+            StartCall = startCall;
+
+        }
+
+    }
+}
